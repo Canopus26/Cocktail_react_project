c9f5fde6785ed64b0a4a19e37fcfc49f793cacbc
Canopus26 <sohilkhokhar26@gmail.com>
2021-02-10 17:01:06 +0530

    intial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        numquam nulla exercitationem esse. Labore, at velit excepturi atque
+        maxime ipsam omnis rem veritatis ullam odit provident in temporibus
+        illum sunt?
+      </p>
+    </section>
+  );
+};
+
+export default About;
